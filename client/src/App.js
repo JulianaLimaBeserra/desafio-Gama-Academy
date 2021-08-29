31b5e80260d2b4fe4d81c54b16f16045722b2894
Juliana Lima <julianalimab88@gmail.com>
2021-08-29 13:27:52 -0300

    Layout utilizado com framewwork Material-ui adicionado inicialmente para home

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-//import Inicio from './pages/client/painel';
+import Inicio from './pages/client/painel';
 
 
 
 function App() {
   return (
     <div className="App">
-       <header className="App-header">
+       {/* <header className="App-header">
         <h1> Formulário </h1>
-      </header> 
+      </header>  */}
+      <Inicio/>
     </div>
   );
 }
